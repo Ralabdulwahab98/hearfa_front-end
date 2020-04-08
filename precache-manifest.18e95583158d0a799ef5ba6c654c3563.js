850f14d01728a3dc5d6cd5d2b591d73a5d5bb74f
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-04-09 00:26:53 +0300

    Updates

---
+++ precache-manifest.18e95583158d0a799ef5ba6c654c3563.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa6c6f41c2444c32516da6d916fb29f",
+    "url": "/hearfa_front-end/index.html"
+  },
+  {
+    "revision": "7923003c49f9b230784d",
+    "url": "/hearfa_front-end/static/css/main.bcdd9193.chunk.css"
+  },
+  {
+    "revision": "e1736915ba5469bb81ce",
+    "url": "/hearfa_front-end/static/js/2.9ceeb687.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hearfa_front-end/static/js/2.9ceeb687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7923003c49f9b230784d",
+    "url": "/hearfa_front-end/static/js/main.ed952e13.chunk.js"
+  },
+  {
+    "revision": "cf0727415b78f6b3d279",
+    "url": "/hearfa_front-end/static/js/runtime-main.1e5298ae.js"
+  },
+  {
+    "revision": "9b15a61a1d55d8ecacd5cad872c1bf28",
+    "url": "/hearfa_front-end/static/media/Carpenter.9b15a61a.png"
+  },
+  {
+    "revision": "c03e6872c50f4edc9613fcb9d0f65b2e",
+    "url": "/hearfa_front-end/static/media/Services.c03e6872.jpg"
+  },
+  {
+    "revision": "b3e1a972ab066e1b9e4c3158c9a9d757",
+    "url": "/hearfa_front-end/static/media/allw.b3e1a972.jpg"
+  },
+  {
+    "revision": "abae1399a97f7e3e4b3aafcf8c664cff",
+    "url": "/hearfa_front-end/static/media/elico.abae1399.png"
+  },
+  {
+    "revision": "85d3cc61dfa578bd20d5871485549729",
+    "url": "/hearfa_front-end/static/media/history.85d3cc61.png"
+  },
+  {
+    "revision": "3f4b0cbc2708c2915daca41c2d0871f4",
+    "url": "/hearfa_front-end/static/media/hourglass.3f4b0cbc.png"
+  },
+  {
+    "revision": "3edb4fe56227c6ded4ed8806d2d8a294",
+    "url": "/hearfa_front-end/static/media/man.3edb4fe5.png"
+  },
+  {
+    "revision": "8218be90884cb3f5b72667fb0c0b5b64",
+    "url": "/hearfa_front-end/static/media/profile.8218be90.png"
+  },
+  {
+    "revision": "147a950b656458257ef281a6393b4f54",
+    "url": "/hearfa_front-end/static/media/share.147a950b.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hearfa_front-end/precache-manifest.18e95583158d0a799ef5ba6c654c3563.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hearfa_front-end/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
